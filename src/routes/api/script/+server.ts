5c230b7f3b498057fce2fe0af9baef6c611292ad
Bug <151166407+BugONSnap@users.noreply.github.com>
2026-04-24 10:11:17 +0800

    remove the problem

--- src/routes/api/script/+server.ts
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch('https://umami.is/script.js');
-  const text = await response.text();
-
-  res.setHeader('Content-Type', 'application/javascript');
-  res.send(text);
-}
